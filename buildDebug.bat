512bb5260fdec316cc33fe3b51dd6054821c6ade
William Emerison Six <billsix@gmail.com>
2020-06-02 18:54:04 -0400

    build in Debug mode using visual studio

--- buildDebug.bat
+++ buildDebug.bat
@@ -4,5 +4,3 @@ cd build
 REM create the visual studio solution and project files
 cmake -DCMAKE_INSTALL_PREFIX=..\buildInstall ..\
 cmake --build . --target INSTALL --config Debug
-cd ..\buildInstall
-.\bin\craft.exe
