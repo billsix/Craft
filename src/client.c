billsix/Craft

commit 1ccce501c253288b43bfa441ba3cf14a51ab8bb1

ran clang-format

src/client.c
@@ -1,6 +1,6 @@
 #ifdef _WIN32
-#include <winsock2.h>
 #include <windows.h>
+#include <winsock2.h>
 #define close closesocket
 #define sleep Sleep
 #else
@@ -8,11 +8,11 @@
 #include <unistd.h>
 #endif
 
+#include "client.h"
+#include "tinycthread.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "client.h"
-#include "tinycthread.h"
 
 #define QUEUE_SIZE 1048576
 #define RECV_SIZE 4096
@@ -27,17 +27,11 @@ static int qsize = 0;
 static thrd_t recv_thread;
 static mtx_t mutex;
 
-void client_enable() {
-  client_enabled = 1;
-}
+void client_enable() { client_enabled = 1; }
 
-void client_disable() {
-  client_enabled = 0;
-}
+void client_disable() { client_enabled = 0; }
 
-int get_client_enabled() {
-  return client_enabled;
-}
+int get_client_enabled() { return client_enabled; }
 
 int client_sendall(int sd, char *data, int length) {
   if (!client_enabled) {
@@ -89,16 +83,17 @@ void client_position(float x, float y, float z, float rx, float ry) {
     return;
   }
   static float px, py, pz, prx, pry = 0;
-  float distance =
-    (px - x) * (px - x) +
-    (py - y) * (py - y) +
-    (pz - z) * (pz - z) +
-    (prx - rx) * (prx - rx) +
-    (pry - ry) * (pry - ry);
+  float distance = (px - x) * (px - x) + (py - y) * (py - y) +
+                   (pz - z) * (pz - z) + (prx - rx) * (prx - rx) +
+                   (pry - ry) * (pry - ry);
   if (distance < 0.0001) {
     return;
   }
-  px = x; py = y; pz = z; prx = rx; pry = ry;
+  px = x;
+  py = y;
+  pz = z;
+  prx = rx;
+  pry = ry;
   char buffer[1024];
   snprintf(buffer, 1024, "P,%.2f,%.2f,%.2f,%.2f,%.2f\n", x, y, z, rx, ry);
   client_send(buffer);
@@ -184,8 +179,7 @@ int recv_worker(void *arg) {
       if (running) {
         perror("recv");
         exit(1);
-      }
-      else {
+      } else {
         break;
       }
     }

src/auth.c
@@ -1,8 +1,8 @@
+#include "auth.h"
 #include <curl/curl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "auth.h"
 
 #define MAX_POST_LENGTH 1024
 #define MAX_RESPONSE_LENGTH 1024
@@ -18,9 +18,8 @@ size_t write_function(char *data, size_t size, size_t count, void *arg) {
   return length;
 }
 
-int get_access_token(
-                     char *result, int length, char *username, char *identity_token)
-{
+int get_access_token(char *result, int length, char *username,
+                     char *identity_token) {
   static char url[] = "https://craft.michaelfogleman.com/api/1/identity";
   strncpy(result, "", length);
   CURL *curl = curl_easy_init();
@@ -28,8 +27,8 @@ int get_access_token(
     char post[MAX_POST_LENGTH] = {0};
     char response[MAX_RESPONSE_LENGTH] = {0};
     long http_code = 0;
-    snprintf(post, MAX_POST_LENGTH, "username=%s&identity_token=%s",
-             username, identity_token);
+    snprintf(post, MAX_POST_LENGTH, "username=%s&identity_token=%s", username,
+             identity_token);
 #ifdef _WIN32
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
 #endif

src/auth.h
@@ -1,7 +1,7 @@
 #ifndef _auth_h_
 #define _auth_h_
 
-int get_access_token(
-                     char *result, int length, char *username, char *identity_token);
+int get_access_token(char *result, int length, char *username,
+                     char *identity_token);
 
 #endif

src/cube.h
@@ -1,34 +1,26 @@
 #ifndef _cube_h_
 #define _cube_h_
 
-void make_cube_faces(
-                     float *data, float ao[6][4], float light[6][4],
-                     int left, int right, int top, int bottom, int front, int back,
-                     int wleft, int wright, int wtop, int wbottom, int wfront, int wback,
-                     float x, float y, float z, float n);
+void make_cube_faces(float *data, float ao[6][4], float light[6][4], int left,
+                     int right, int top, int bottom, int front, int back,
+                     int wleft, int wright, int wtop, int wbottom, int wfront,
+                     int wback, float x, float y, float z, float n);
 
-void make_cube(
-               float *data, float ao[6][4], float light[6][4],
-               int left, int right, int top, int bottom, int front, int back,
-               float x, float y, float z, float n, int w);
+void make_cube(float *data, float ao[6][4], float light[6][4], int left,
+               int right, int top, int bottom, int front, int back, float x,
+               float y, float z, float n, int w);
 
-void make_plant(
-                float *data, float ao, float light,
-                float px, float py, float pz, float n, int w, float rotation);
+void make_plant(float *data, float ao, float light, float px, float py,
+                float pz, float n, int w, float rotation);
 
-void make_player(
-                 float *data,
-                 float x, float y, float z, float rx, float ry);
+void make_player(float *data, float x, float y, float z, float rx, float ry);
 
-void make_cube_wireframe(
-                         float *data, float x, float y, float z, float n);
+void make_cube_wireframe(float *data, float x, float y, float z, float n);
 
-void make_character(
-                    float *data,
-                    float x, float y, float n, float m, char c);
+void make_character(float *data, float x, float y, float n, float m, char c);
 
-void make_character_3d(
-                       float *data, float x, float y, float z, float n, int face, char c);
+void make_character_3d(float *data, float x, float y, float z, float n,
+                       int face, char c);
 
 void make_sphere(float *data, float r, int detail);
 

src/db.c
@@ -1,8 +1,8 @@
-#include <string.h>
 #include "db.h"
 #include "ring.h"
 #include "sqlite3.h"
 #include "tinycthread.h"
+#include <string.h>
 
 static int db_enabled = 0;
 
@@ -24,127 +24,130 @@ static mtx_t mtx;
 static cnd_t cnd;
 static mtx_t load_mtx;
 
-void db_enable() {
-  db_enabled = 1;
-}
+void db_enable() { db_enabled = 1; }
 
-void db_disable() {
-  db_enabled = 0;
-}
+void db_disable() { db_enabled = 0; }
 
-int get_db_enabled() {
-  return db_enabled;
-}
+int get_db_enabled() { return db_enabled; }
 
 int db_init(char *path) {
   if (!db_enabled) {
     return 0;
   }
   static const char *create_query =
-    "attach database 'auth.db' as auth;"
-    "create table if not exists auth.identity_token ("
-    "   username text not null,"
-    "   token text not null,"
-    "   selected int not null"
-    ");"
-    "create unique index if not exists auth.identity_token_username_idx"
-    "   on identity_token (username);"
-    "create table if not exists positionAndOrientation ("
-    "   x float not null,"
-    "   y float not null,"
-    "   z float not null,"
-    "   rx float not null,"
-    "   ry float not null"
-    ");"
-    "create table if not exists block ("
-    "    p int not null,"
-    "    q int not null,"
-    "    x int not null,"
-    "    y int not null,"
-    "    z int not null,"
-    "    w int not null"
-    ");"
-    "create table if not exists light ("
-    "    p int not null,"
-    "    q int not null,"
-    "    x int not null,"
-    "    y int not null,"
-    "    z int not null,"
-    "    w int not null"
-    ");"
-    "create table if not exists key ("
-    "    p int not null,"
-    "    q int not null,"
-    "    key int not null"
-    ");"
-    "create table if not exists sign ("
-    "    p int not null,"
-    "    q int not null,"
-    "    x int not null,"
-    "    y int not null,"
-    "    z int not null,"
-    "    face int not null,"
-    "    text text not null"
-    ");"
-    "create unique index if not exists block_pqxyz_idx on block (p, q, x, y, z);"
-    "create unique index if not exists light_pqxyz_idx on light (p, q, x, y, z);"
-    "create unique index if not exists key_pq_idx on key (p, q);"
-    "create unique index if not exists sign_xyzface_idx on sign (x, y, z, face);"
-    "create index if not exists sign_pq_idx on sign (p, q);";
+      "attach database 'auth.db' as auth;"
+      "create table if not exists auth.identity_token ("
+      "   username text not null,"
+      "   token text not null,"
+      "   selected int not null"
+      ");"
+      "create unique index if not exists auth.identity_token_username_idx"
+      "   on identity_token (username);"
+      "create table if not exists positionAndOrientation ("
+      "   x float not null,"
+      "   y float not null,"
+      "   z float not null,"
+      "   rx float not null,"
+      "   ry float not null"
+      ");"
+      "create table if not exists block ("
+      "    p int not null,"
+      "    q int not null,"
+      "    x int not null,"
+      "    y int not null,"
+      "    z int not null,"
+      "    w int not null"
+      ");"
+      "create table if not exists light ("
+      "    p int not null,"
+      "    q int not null,"
+      "    x int not null,"
+      "    y int not null,"
+      "    z int not null,"
+      "    w int not null"
+      ");"
+      "create table if not exists key ("
+      "    p int not null,"
+      "    q int not null,"
+      "    key int not null"
+      ");"
+      "create table if not exists sign ("
+      "    p int not null,"
+      "    q int not null,"
+      "    x int not null,"
+      "    y int not null,"
+      "    z int not null,"
+      "    face int not null,"
+      "    text text not null"
+      ");"
+      "create unique index if not exists block_pqxyz_idx on block (p, q, x, y, "
+      "z);"
+      "create unique index if not exists light_pqxyz_idx on light (p, q, x, y, "
+      "z);"
+      "create unique index if not exists key_pq_idx on key (p, q);"
+      "create unique index if not exists sign_xyzface_idx on sign (x, y, z, "
+      "face);"
+      "create index if not exists sign_pq_idx on sign (p, q);";
   static const char *insert_block_query =
-    "insert or replace into block (p, q, x, y, z, w) "
-    "values (?, ?, ?, ?, ?, ?);";
+      "insert or replace into block (p, q, x, y, z, w) "
+      "values (?, ?, ?, ?, ?, ?);";
   static const char *insert_light_query =
-    "insert or replace into light (p, q, x, y, z, w) "
-    "values (?, ?, ?, ?, ?, ?);";
+      "insert or replace into light (p, q, x, y, z, w) "
+      "values (?, ?, ?, ?, ?, ?);";
   static const char *insert_sign_query =
-    "insert or replace into sign (p, q, x, y, z, face, text) "
-    "values (?, ?, ?, ?, ?, ?, ?);";
+      "insert or replace into sign (p, q, x, y, z, face, text) "
+      "values (?, ?, ?, ?, ?, ?, ?);";
   static const char *delete_sign_query =
-    "delete from sign where x = ? and y = ? and z = ? and face = ?;";
+      "delete from sign where x = ? and y = ? and z = ? and face = ?;";
   static const char *delete_signs_query =
-    "delete from sign where x = ? and y = ? and z = ?;";
+      "delete from sign where x = ? and y = ? and z = ?;";
   static const char *load_blocks_query =
-    "select x, y, z, w from block where p = ? and q = ?;";
+      "select x, y, z, w from block where p = ? and q = ?;";
   static const char *load_lights_query =
-    "select x, y, z, w from light where p = ? and q = ?;";
+      "select x, y, z, w from light where p = ? and q = ?;";
   static const char *load_signs_query =
-    "select x, y, z, face, text from sign where p = ? and q = ?;";
+      "select x, y, z, face, text from sign where p = ? and q = ?;";
   static const char *get_key_query =
-    "select key from key where p = ? and q = ?;";
-  static const char *set_key_query =
-    "insert or replace into key (p, q, key) "
-    "values (?, ?, ?);";
+      "select key from key where p = ? and q = ?;";
+  static const char *set_key_query = "insert or replace into key (p, q, key) "
+                                     "values (?, ?, ?);";
   int rc;
   rc = sqlite3_open(path, &db);
-  if (rc) return rc;
+  if (rc)
+    return rc;
   rc = sqlite3_exec(db, create_query, NULL, NULL, NULL);
-  if (rc) return rc;
-  rc = sqlite3_prepare_v2(
-                          db, insert_block_query, -1, &insert_block_stmt, NULL);
-  if (rc) return rc;
-  rc = sqlite3_prepare_v2(
-                          db, insert_light_query, -1, &insert_light_stmt, NULL);
-  if (rc) return rc;
-  rc = sqlite3_prepare_v2(
-                          db, insert_sign_query, -1, &insert_sign_stmt, NULL);
-  if (rc) return rc;
-  rc = sqlite3_prepare_v2(
-                          db, delete_sign_query, -1, &delete_sign_stmt, NULL);
-  if (rc) return rc;
-  rc = sqlite3_prepare_v2(
-                          db, delete_signs_query, -1, &delete_signs_stmt, NULL);
-  if (rc) return rc;
+  if (rc)
+    return rc;
+  rc = sqlite3_prepare_v2(db, insert_block_query, -1, &insert_block_stmt, NULL);
+  if (rc)
+    return rc;
+  rc = sqlite3_prepare_v2(db, insert_light_query, -1, &insert_light_stmt, NULL);
+  if (rc)
+    return rc;
+  rc = sqlite3_prepare_v2(db, insert_sign_query, -1, &insert_sign_stmt, NULL);
+  if (rc)
+    return rc;
+  rc = sqlite3_prepare_v2(db, delete_sign_query, -1, &delete_sign_stmt, NULL);
+  if (rc)
+    return rc;
+  rc = sqlite3_prepare_v2(db, delete_signs_query, -1, &delete_signs_stmt, NULL);
+  if (rc)
+    return rc;
   rc = sqlite3_prepare_v2(db, load_blocks_query, -1, &load_blocks_stmt, NULL);
-  if (rc) return rc;
+  if (rc)
+    return rc;
   rc = sqlite3_prepare_v2(db, load_lights_query, -1, &load_lights_stmt, NULL);
-  if (rc) return rc;
+  if (rc)
+    return rc;
   rc = sqlite3_prepare_v2(db, load_signs_query, -1, &load_signs_stmt, NULL);
-  if (rc) return rc;
+  if (rc)
+    return rc;
   rc = sqlite3_prepare_v2(db, get_key_query, -1, &get_key_stmt, NULL);
-  if (rc) return rc;
+  if (rc)
+    return rc;
   rc = sqlite3_prepare_v2(db, set_key_query, -1, &set_key_stmt, NULL);
-  if (rc) return rc;
+  if (rc)
+    return rc;
   sqlite3_exec(db, "begin;", NULL, NULL, NULL);
   db_worker_start();
   return 0;
@@ -179,17 +182,14 @@ void db_commit() {
   mtx_unlock(&mtx);
 }
 
-void _db_commit() {
-  sqlite3_exec(db, "commit; begin;", NULL, NULL, NULL);
-}
+void _db_commit() { sqlite3_exec(db, "commit; begin;", NULL, NULL, NULL); }
 
 void db_auth_set(char *username, char *identity_token) {
   if (!db_enabled) {
     return;
   }
-  static const char *query =
-    "insert or replace into auth.identity_token "
-    "(username, token, selected) values (?, ?, ?);";
+  static const char *query = "insert or replace into auth.identity_token "
+                             "(username, token, selected) values (?, ?, ?);";
   sqlite3_stmt *stmt;
   sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
   sqlite3_bind_text(stmt, 1, username, -1, NULL);
@@ -206,7 +206,7 @@ int db_auth_select(char *username) {
   }
   db_auth_select_none();
   static const char *query =
-    "update auth.identity_token set selected = 1 where username = ?;";
+      "update auth.identity_token set selected = 1 where username = ?;";
   sqlite3_stmt *stmt;
   sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
   sqlite3_bind_text(stmt, 1, username, -1, NULL);
@@ -219,20 +219,17 @@ void db_auth_select_none() {
   if (!db_enabled) {
     return;
   }
-  sqlite3_exec(db, "update auth.identity_token set selected = 0;",
-               NULL, NULL, NULL);
+  sqlite3_exec(db, "update auth.identity_token set selected = 0;", NULL, NULL,
+               NULL);
 }
 
-int db_auth_get(
-                char *username,
-                char *identity_token, int identity_token_length)
-{
+int db_auth_get(char *username, char *identity_token,
+                int identity_token_length) {
   if (!db_enabled) {
     return 0;
   }
-  static const char *query =
-    "select token from auth.identity_token "
-    "where username = ?;";
+  static const char *query = "select token from auth.identity_token "
+                             "where username = ?;";
   int result = 0;
   sqlite3_stmt *stmt;
   sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
@@ -247,16 +244,13 @@ int db_auth_get(
   return result;
 }
 
-int db_auth_get_selected(
-                         char *username, int username_length,
-                         char *identity_token, int identity_token_length)
-{
+int db_auth_get_selected(char *username, int username_length,
+                         char *identity_token, int identity_token_length) {
   if (!db_enabled) {
     return 0;
   }
-  static const char *query =
-    "select username, token from auth.identity_token "
-    "where selected = 1;";
+  static const char *query = "select username, token from auth.identity_token "
+                             "where selected = 1;";
   int result = 0;
   sqlite3_stmt *stmt;
   sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
@@ -277,8 +271,8 @@ void db_save_state(float x, float y, float z, float rx, float ry) {
   if (!db_enabled) {
     return;
   }
-  static const char *query =
-    "insert into positionAndOrientation (x, y, z, rx, ry) values (?, ?, ?, ?, ?);";
+  static const char *query = "insert into positionAndOrientation (x, y, z, rx, "
+                             "ry) values (?, ?, ?, ?, ?);";
   sqlite3_stmt *stmt;
   sqlite3_exec(db, "delete from positionAndOrientation;", NULL, NULL, NULL);
   sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
@@ -296,7 +290,7 @@ int db_load_state(float *x, float *y, float *z, float *rx, float *ry) {
     return 0;
   }
   static const char *query =
-    "select x, y, z, rx, ry from positionAndOrientation;";
+      "select x, y, z, rx, ry from positionAndOrientation;";
   int result = 0;
   sqlite3_stmt *stmt;
   sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
@@ -354,9 +348,8 @@ void _db_insert_light(int p, int q, int x, int y, int z, int w) {
   sqlite3_step(insert_light_stmt);
 }
 
-void db_insert_sign(
-                    int p, int q, int x, int y, int z, int face, const char *text)
-{
+void db_insert_sign(int p, int q, int x, int y, int z, int face,
+                    const char *text) {
   if (!db_enabled) {
     return;
   }
@@ -449,8 +442,7 @@ void db_load_signs(SignList *list, int p, int q) {
     int y = sqlite3_column_int(load_signs_stmt, 1);
     int z = sqlite3_column_int(load_signs_stmt, 2);
     int face = sqlite3_column_int(load_signs_stmt, 3);
-    const char *text = (const char *)sqlite3_column_text(
-                                                         load_signs_stmt, 4);
+    const char *text = (const char *)sqlite3_column_text(load_signs_stmt, 4);
     sign_list_add(list, x, y, z, face, text);
   }
 }

commit 2ef58d49f975e22163b2f79b5ebc0cc58c2f3afc

Fixed build failure on Visual Studio.
Problem was introduced in 1ccce501c253288b43bfa441ba3cf14a51ab8bb1
due to clang-format.

src/client.c
@@ -1,6 +1,7 @@
 #ifdef _WIN32
-#include <windows.h>
 #include <winsock2.h>
+
+#include <windows.h>
 #define close closesocket
 #define sleep Sleep
 #else

src/gl_render.c
@@ -1,8 +1,10 @@
 #define GL3W_IMPLEMENTATION 1
-#include "gl3w.h"
 
-// N.B - this whitespace is required so that clang-format
-// does not break the build.
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "gl3w.h"
 
 #include "tinycthread.h"
 #include <GLFW/glfw3.h>

src/main.c
@@ -1,7 +1,11 @@
-#include <GLFW/glfw3.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-// N.B - this whitespace is required so that clang-format
-// does not break the build.
+#include <GLFW/glfw3.h>
 
 #include "config.h"
 
@@ -29,12 +33,6 @@
 
 #include "world.h"
 #include <curl/curl.h>
-#include <math.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>