80f55278afeddc9e7e1b7722f6f7276bf7699816
William Emerison Six <billsix@gmail.com>
2020-10-28 17:39:25 -0400

    fixed const problems

--- src/vulkan_render.c
+++ src/vulkan_render.c
@@ -54,7 +54,7 @@ void vulkan_disable_scissor_test() {}
 void vulkan_scissor(uint32_t x_min, uint32_t y_min, uint32_t x_width,
                     uint32_t y_height) {}
 
-uint32_t vulkan_gen_buffer(size_t size, float *data) { return 0; }
+uint32_t vulkan_gen_buffer(size_t size, const float *const data) { return 0; }
 
 void vulkan_del_buffer(uint32_t buffer) {}
 
@@ -76,40 +76,44 @@ void vulkan_initiliaze_global_state() {}
 
 void vulkan_initiliaze_textures() {}
 
-void vulkan_setup_render_chunks(float *matrix,
-                                PositionAndOrientation *positionAndOrientation,
-                                float light) {}
+void vulkan_setup_render_chunks(
+    const float *const matrix,
+    const PositionAndOrientation *const positionAndOrientation, float light) {}
 
-void vulkan_render_chunk(Chunk *chunk) {}
+void vulkan_render_chunk(const Chunk *const chunk) {}
 
 void vulkan_draw_triangles_3d_text(uint32_t buffer, int count) {}
 
-void vulkan_setup_render_signs(float *matrix) {}
+void vulkan_setup_render_signs(const float *const matrix) {}
 
-void vulkan_render_signs(Chunk *chunk) {}
+void vulkan_render_signs(const Chunk *const chunk) {}
 
-void vulkan_render_sign(float *matrix, int x, int y, int z, int face) {}
+void vulkan_render_sign(const float *const matrix, int x, int y, int z,
+                        int face) {}
 
 void vulkan_setup_render_players(
-    float *matrix, PositionAndOrientation *positionAndOrientation) {}
+    const float *const matrix,
+    const PositionAndOrientation *const positionAndOrientation) {}
 
-void vulkan_render_player(Player *other_player) {}
+void vulkan_render_player(const Player *const other_player) {}
 
-void vulkan_render_sky(uint32_t buffer, float *matrix) {}
+void vulkan_render_sky(uint32_t buffer, const float *const matrix) {}
 
 void vulkan_draw_lines(uint32_t buffer, int components, int count) {}
 
-void vulkan_render_wireframe(float *matrix, int hx, int hy, int hz) {}
+void vulkan_render_wireframe(const float *const matrix, int hx, int hy,
+                             int hz) {}
 
-void vulkan_render_text(float *matrix, int justify, float x, float y, float n,
-                        char *text) {}
+void vulkan_render_text(const float *const matrix, int justify, float x,
+                        float y, float n, const char *const text) {}
 
-void vulkan_render_item(float *matrix) {}
+void vulkan_render_item(const float *const matrix) {}
 
 void vulkan_render_plant(uint32_t plant_buffer) {}
 
 void vulkan_render_cube(uint32_t cube_buffer) {}
 
-void vulkan_render_crosshairs(uint32_t crosshair_buffer, float *matrix) {}
+void vulkan_render_crosshairs(uint32_t crosshair_buffer,
+                              const float *const matrix) {}
 
 #endif
